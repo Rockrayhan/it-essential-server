6b4803d66a4f3165c4341177842ac06293a9178a
Rockrayhan <khayrulalamdict@gmail.com>
2022-01-05 22:47:55 +0600

    push

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const ObjectId = require('mongodb').ObjectId;
 const res = require('express/lib/response');
+const fileUpload = require('express-fileupload');
+
 
 app.use(cors());
 app.use(express.json());
-
+app.use(fileUpload()) ;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.shqkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -77,6 +79,16 @@ async function run() {
       const result = await ordersCollection.insertOne(order)
       res.json(result)
     });
+
+
+
+    // GET ( Order by ID ) for payment
+    app.get('/orders/:id', async(req, res) => {
+      const id = req.params.id ;
+      const query = {_id: ObjectId(id)} ;
+      const result = await ordersCollection.findOne(query);
+      res.json(result);
+    })
 
 
     // save user in DB
